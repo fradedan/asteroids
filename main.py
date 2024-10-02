c2f2b3175b3685a97e08ee8b52a0067d0604ac8b
dfrade <fradedan@gmail.com>
2024-10-02 15:30:26 +0800

    added cyrcleshape.py and setup vsl

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ import pygame
 from constants import *
 
 
-
-
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print("Starting asteroids!")
     print (f"Screen width: {SCREEN_WIDTH}")
     print (f"Screen height: {SCREEN_HEIGHT}")
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(000)
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick()/1000
+
 
 if __name__ == "__main__":
     main()
